d9cf9d56f4ac843ce5a7d804d4530cf7eb024609
Islam Turky <iturky06#gmail.com>
2024-03-14 03:44:20 +0200

    target=blank

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Kiriazi from './pages/Kiriazi.jsx'
+import Samsung from './pages/Samsung.jsx'
+import Lg from './pages/LG.jsx'
+import White from './pages/White.jsx'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Spidal from './components/Spidal';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/kiriazi" element={<Kiriazi />} />
+        <Route path="/lg" element={<Lg />} />
+        <Route path="/samsung" element={<Samsung />} />
+        <Route path="/white-whale" element={<White />} />
+      </Routes>
+    </BrowserRouter>
+    <Spidal />
+    <Footer />
   </React.StrictMode>,
 )
---
+++ src/pages/Kiriazi.jsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+const Kiriazi = () => {
+    return(
+        <Box sx={{ marginTop: '120px', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center' }}>
+            <Typography variant="h1" sx={{ textAlign: 'center' }}>كريازي</Typography>
+            <Typography variant="h4" sx={{ textAlign: 'center' }}>كريازي هو مركز صيانة تقنية للتقنيات الأساسية والأساسية الأساسية</Typography>
+            <Typography variant="h5" sx={{ textAlign: 'center' }}>اكتشف جميع الاجهزة المنزلية من كريازي من غسالات ملابس و بوتاجازات بلت ان وغسالات اطباق و ميكروويف وشفاطات بلت ان بتكنولوجيا ايطالية حديثة بافضل الاسعار مع تركيب مجاني وصيانة لجميع الاجهزة المنزلية من كريازي</Typography>
+        </Box>
+    )
+};
+
+export default Kiriazi;
